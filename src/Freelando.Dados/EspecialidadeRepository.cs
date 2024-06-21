c90ca1432d6854f7d0455628fe514f143ef58780
bessax <andrebessax@gmail.com>
2024-06-21 14:55:26 -0300

    #update: Video 4.1

---
+++ src/Freelando.Dados/EspecialidadeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Freelando.Dados.Repository;
+using Freelando.Modelo;
+
+namespace Freelando.Dados;
+public class EspecialidadeRepository : Repository<Especialidade>, IEspecialidadeRepository
+{
+    public EspecialidadeRepository(FreelandoContext context) : base(context)
+    {
+    }
+}
---
+++ src/Freelando.Dados/IEspecialidadeRepository.cs
@@ -0,0 +1,8 @@
+﻿using Freelando.Dados.Repository;
+using Freelando.Modelo;
+
+namespace Freelando.Dados;
+public interface IEspecialidadeRepository : IRepository<Especialidade>
+{
+}
+
